ad78620b0233d954b3d5b20c2e674a6200b69e71
REACHSLEGENDA <reachslegenda@users.noreply.github.com>
2026-07-16 20:38:12 -0600

    style: replace plant-vase with user tooth plant image and keep unified gradient layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ export default function Home() {
         >
           {/* Plant Decoration */}
           <div className="absolute top-1/2 -translate-y-1/2 left-0 w-[55%] md:w-[65%] h-full pointer-events-none mix-blend-multiply opacity-60 z-0 flex items-end justify-start pb-0 pl-0">
-            <img src="/images/plant-vase.png" alt="" className="w-full h-auto max-h-[110%] object-contain object-left-bottom" />
+            <img src="/images/bg-hero.png" alt="" className="w-full h-auto max-h-[110%] object-contain object-left-bottom" />
           </div>
           
           <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-white to-transparent"></div>
@@ -107,7 +107,7 @@ export default function Home() {
         >
           {/* Plant Decoration (Flipped) */}
           <div className="absolute top-1/2 -translate-y-1/2 right-0 w-[55%] md:w-[65%] h-full pointer-events-none mix-blend-multiply opacity-60 z-0 flex items-end justify-end pb-0 pr-0">
-            <img src="/images/plant-vase.png" alt="" className="w-full h-auto max-h-[110%] object-contain object-right-bottom transform -scale-x-100" />
+            <img src="/images/bg-hero.png" alt="" className="w-full h-auto max-h-[110%] object-contain object-right-bottom transform -scale-x-100" />
           </div>
 
           <div className="absolute inset-0 opacity-10 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-white to-transparent z-0"></div>
